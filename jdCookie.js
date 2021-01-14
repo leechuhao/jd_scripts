3fa3962ca2e8d6974692183def7c895c13d5b750
lichuhao <1010654308@qq.com>
2021-01-14 09:59:24 +0800

    Update jdCookie.js

--- jdCookie.js
+++ jdCookie.js
@@ -4,7 +4,7 @@
 //此处填写京东账号cookie。
 //注：github action用户cookie填写到Settings-Secrets里面，新增JD_COOKIE，多个账号的cookie使用`&`隔开或者换行
 let CookieJDs = [
-  '',//账号一ck,例:pt_key=XXX;pt_pin=XXX;
+  'pt_key=AAJf_pj_ADDdo4hLQPdfJmv6QyXx8Ei7-PDk89v2oRCyZGPmRiCy-5BPeK7dzP9NcYrYSYmV44w;&pt_pin=jd_6e5155d9480d8;',//账号一ck,例:pt_key=XXX;pt_pin=XXX;
   '',//账号二ck,例:pt_key=XXX;pt_pin=XXX;如有更多,依次类推
 ]
 // 判断github action里面是否有京东ck
